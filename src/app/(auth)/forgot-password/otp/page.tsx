cbe30e824010c1d30504ae7684f92a60aade7c29
popo <yosita.rd@gmail.com>
2024-08-25 00:03:32 +0700

    Add authentication pages

---
+++ src/app/(auth)/forgot-password/otp/page.tsx
@@ -0,0 +1,6 @@
+export default function OtpPage() {
+  return (
+    <div>
+    </div>
+  );
+}
---
+++ src/app/(auth)/signup/success/page.tsx
@@ -0,0 +1,6 @@
+export default function SignupSuccessPage() {
+  return (
+    <div>
+    </div>
+  );
+}
